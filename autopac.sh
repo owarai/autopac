5be8c9f2b8bb4e5d2316e453d3598284617ff7dc
max <dingfengwang@yunify.com>
2020-05-28 12:17:23 +0800

    Add remote

--- autopac.sh
+++ autopac.sh
@@ -6,3 +6,11 @@ git add --update
 today="$(date +'%m/%d/%Y')"
 
 git commit -m"updated in $today"
+
+if git remote get-url origin; then
+  git push origin master:master
+else
+  git remote add origin git@github.com:owarai/autopac.git
+  git branch -u origin/master master
+  git push origin master:master
+fi
