65a05b9c9d1ffe4cb4be7b6f751d771bb47b1b1a
max <dingfengwang@yunify.com>
2020-05-28 12:22:13 +0800

    updated in 05/28/2020

--- autopac.sh
+++ autopac.sh
@@ -1,4 +1,6 @@
-https_proxy=http://172.16.80.2:1090 curl -s https://raw.githubusercontent.com/pexcn/daily/gh-pages/pac/whitelist.pac \
+export https_proxy=http://172.16.80.2:1090
+
+curl -s https://raw.githubusercontent.com/pexcn/daily/gh-pages/pac/whitelist.pac \
  | sed 's/127.0.0.1:1080/172.16.80.2:1080/1' | tee pac.txt
 
 git add --update
