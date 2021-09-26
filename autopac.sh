7a4c705cb7cb2a3ac5270ff74fddf2b4e7e0ca4c
max <dingfengwang@yunify.com>
2021-09-26 21:05:07 +0800

    updated in 09/26/2021

--- autopac.sh
+++ autopac.sh
@@ -1,7 +1,10 @@
+#!/usr/bin/bash
+#
+
 export https_proxy=http://172.16.80.2:1090
 
-curl -s https://raw.githubusercontent.com/pexcn/daily/gh-pages/pac/whitelist.pac \
- | sed 's/127.0.0.1:1080/172.16.80.2:1080/1' | tee pac.txt
+curl -s https://raw.githubusercontent.com/pexcn/daily/gh-pages/pac/whitelist.pac |
+    sed 's/127.0.0.1:1080/172.16.80.2:1080/1' | tee proxy.pac
 
 git add --update
 
@@ -10,9 +13,9 @@ today="$(date +'%m/%d/%Y')"
 git commit -m"updated in $today"
 
 if git remote get-url origin; then
-  git push origin master:master
+    git push origin master:master
 else
-  git remote add origin git@github.com:owarai/autopac.git
-  git branch -u origin/master master
-  git push origin master:master
+    git remote add origin git@github.com:owarai/autopac.git
+    git branch -u origin/master master
+    git push origin master:master
 fi
